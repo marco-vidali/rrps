708b2fd0002eb619c893c2f71942b69209afe9de
marco-vidali <marcovidali@outlook.com>
2026-04-13 18:05:43 +0200

    Divide code into functions

--- src/main.rs
+++ src/main.rs
@@ -22,65 +22,73 @@ fn main() {
     println!("Welcome to RRPS (Rust Rock-Paper-Scissors)!");
 
     loop {
-        // Get a valid move from the user
-        let user_move = loop {
-            println!("Choose a move:");
-            println!("1) Rock");
-            println!("2) Paper");
-            println!("3) Scissors");
+        let user_move = get_user_move();
 
-            let mut user_move = String::new();
+        let computer_move = calculate_computer_move();
+        println!("The user chose {computer_move}");
 
-            io::stdin()
-                .read_line(&mut user_move)
-                .expect("Failed to read user input");
+        determine_winner(&user_move, &computer_move);
+    }
+}
 
-            let user_move: i8 = match user_move.trim().parse() {
-                Ok(user_move) => user_move,
-                Err(_) => {
-                    println!("Please enter a number");
-                    continue;
-                }
-            };
+fn get_user_move() -> Move {
+    loop {
+        println!("Choose a move:");
+        println!("1) Rock");
+        println!("2) Paper");
+        println!("3) Scissors");
 
-            break match user_move {
-                1 => Move::Rock,
-                2 => Move::Paper,
-                3 => Move::Scissors,
-                _ => {
-                    println!("Please enter a valid choice");
-                    continue;
-                }
-            };
+        let mut user_move = String::new();
+
+        io::stdin()
+            .read_line(&mut user_move)
+            .expect("Failed to read user input");
+
+        let user_move: i8 = match user_move.trim().parse() {
+            Ok(user_move) => user_move,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            }
         };
 
-        // Calculate a random move for the computer
-        let computer_move = match rand::rng().random_range(1..=3) {
+        break match user_move {
             1 => Move::Rock,
             2 => Move::Paper,
             3 => Move::Scissors,
-            _ => unreachable!("random_range(1..=3) should only return 1, 2 or 3"),
+            _ => {
+                println!("Please enter a valid choice");
+                continue;
+            }
         };
-
-        println!("The user chose {computer_move}");
-
-        // Determine winner
-        match user_move {
-            Move::Rock => match computer_move {
-                Move::Rock => println!("Tie!"),
-                Move::Paper => println!("You lost!"),
-                Move::Scissors => println!("You won!"),
-            },
-            Move::Paper => match computer_move {
-                Move::Rock => println!("You won!"),
-                Move::Paper => println!("Tie!"),
-                Move::Scissors => println!("You lost!"),
-            },
-            Move::Scissors => match computer_move {
-                Move::Rock => println!("You lost!"),
-                Move::Paper => println!("You won!"),
-                Move::Scissors => println!("Tie!"),
-            },
-        }
     }
+}
+
+fn calculate_computer_move() -> Move {
+    match rand::rng().random_range(1..=3) {
+        1 => Move::Rock,
+        2 => Move::Paper,
+        3 => Move::Scissors,
+        _ => unreachable!("random_range(1..=3) should only return 1, 2 or 3"),
+    }
+}
+
+fn determine_winner(user_move: &Move, computer_move: &Move) {
+    match user_move {
+        Move::Rock => match computer_move {
+            Move::Rock => println!("Tie!"),
+            Move::Paper => println!("You lost!"),
+            Move::Scissors => println!("You won!"),
+        },
+        Move::Paper => match computer_move {
+            Move::Rock => println!("You won!"),
+            Move::Paper => println!("Tie!"),
+            Move::Scissors => println!("You lost!"),
+        },
+        Move::Scissors => match computer_move {
+            Move::Rock => println!("You lost!"),
+            Move::Paper => println!("You won!"),
+            Move::Scissors => println!("Tie!"),
+        },
+    };
 }
