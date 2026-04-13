4a71541bcfb243dbac17d94b6ea6ef3d6d22e48b
marco-vidali <marcovidali@outlook.com>
2026-04-13 17:46:12 +0200

    Get valid move for the user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::io;
+
+#[derive(Debug)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
 fn main() {
-    println!("Hello, world!");
+    clearscreen::clear().expect("Failed to clear screen");
+    println!("Welcome to RRPS (Rust Rock-Paper-Scissors)!");
+
+    loop {
+        // Get a valid move from the user
+        let user_move = loop {
+            println!("Choose a move:");
+            println!("1) Rock");
+            println!("2) Paper");
+            println!("3) Scissors");
+
+            let mut user_move = String::new();
+
+            io::stdin()
+                .read_line(&mut user_move)
+                .expect("Failed to read user input");
+
+            let user_move: i8 = match user_move.trim().parse() {
+                Ok(user_move) => user_move,
+                Err(_) => {
+                    println!("Please enter a number");
+                    continue;
+                }
+            };
+
+            break match user_move {
+                1 => Move::Rock,
+                2 => Move::Paper,
+                3 => Move::Scissors,
+                _ => {
+                    println!("Please enter a valid choice");
+                    continue;
+                }
+            };
+        };
+    }
 }
